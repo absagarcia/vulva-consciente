5e523aafd512c6cbc2d7abfa111accce5c3de759
eliecer garcia <eliecer.garcia@exodus.com.mx>
2024-10-15 19:00:41 -0600

    conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+// next.config.mjs
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = "";
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, ""); // Obtener el nombre del repositorio
+    basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    assetPrefix: isGithubActions ? basePath : "",
+    basePath: isGithubActions ? basePath : "",
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
